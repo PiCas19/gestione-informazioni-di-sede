db3da0c42ab00eca001df224ad6a7db7368faa3d
Pierapoalo Casati <pierpaolo.casati@samtrevano.ch>
2021-05-24 12:04:34 +0200

    Creazione views schermo

---
+++ 5_Sito/application/views/schermo/index.php
@@ -0,0 +1,10 @@
+<!-- Parte centrale dello schermo -->
+<div class="container">
+	<div id="reload" class='row'>
+	</div>
+</div>
+ 
+<!-- Script che permette di visualizzare i pdf -->
+<script type="text/javascript" src="<?php  echo URL;?>application/sources/script/pdfViewer.js"></script>
+<!-- Script che permette di aggiornare lo schermo -->
+<script type="text/javascript" src="<?php  echo URL;?>application/sources/script/reload.js"></script>
